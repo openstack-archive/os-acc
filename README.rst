3d44785f2e2b2c2e71e155e65da0cabeeb0679ae
wangzh21 <wangzh21@lenovo.com>
2018-07-17 11:35:40 +0800

    Init os-acc
    1.Imply os-acc to attach or detach accelerator.
    2.This implementation couples with libvirt, we must improve
      it later.
    3.Here is a patch of nova. I hope it can help to review this
      commit.
      https://github.com/wangzh1993/openstack_patch/tree/master/cyborg-os_acc-nova-patch/nova
    4.Add UT
    5.Remove unnecessary file.
    6.Remove unused config in tox.
    
    Change-Id: I140cb63549dbe2f70ac84122c569564193001e18

---
+++ README.rst
@@ -0,0 +1,15 @@
+========================
+Team and repository tags
+========================
+
+======
+os-acc
+======
+
+A library for attaching and detaching accelerators in OpenStack.
+
+* License: Apache License, Version 2.0
+* Documentation:
+* Source:
+* Bugs:
+* Release Notes:
---
+++ doc/source/conf.py
@@ -0,0 +1,54 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -- General configuration ----------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'openstackdocstheme',
+    'reno.sphinxext',
+]
+
+# openstackdocstheme options
+repository_name = 'openstack/os-acc'
+bug_project = 'os-acc'
+bug_tag = ''
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = u'os-acc'
+copyright = u'2016, OpenStack Foundation'
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'openstackdocs'
